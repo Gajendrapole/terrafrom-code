370c77e751d8c5fdd0f5d3eecfc714acd3798f45
Gajendrapole <102959913+Gajendrapole@users.noreply.github.com>
2022-07-05 20:17:46 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -13,21 +13,21 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-# resource "google_compute_instance" "vm_instance" {
-#   name         = "terraform-instance"
-#   machine_type = "f1-micro"
+resource "google_compute_instance" "vm_instance" {
+   name         = "terraform-instance"
+   machine_type = "f1-micro"
 
-#   boot_disk {
-#     initialize_params {
-#       image = "debian-cloud/debian-9"
-#     }
-#  }
+   boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+ }
 
-#   network_interface {
-#     # A default network is created for all GCP projects
-#     network = "default"
-#     access_config {
-#     }
-#   }
-# }
+   network_interface {
+     # A default network is created for all GCP projects
+     network = "default"
+    access_config {
+     }
+  }
+ }
 
